3c24b8383b83944fdf79beef15b64a552ceb96bc
Silviane Bezerra <silvianeraquel@gmail.com>
2022-10-27 16:04:27 -0300

    get_pais_get_cidade_com_axios

--- src/App.js
+++ src/App.js
@@ -1,60 +1,88 @@
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import api from './api';
 
 
-function App() {
+export default function App() {
 
-  const [pais, setPais] = useState('');
-  const [cidade, setCidade] = useState('');
+  const [pais, setPais] = useState([]);
+  const [cidade, setCidade] = useState([]);
 
+  useEffect(() => {
+
+    getPais();
+    getCidade();
+
+ }, [])
+
+function getPais() {
+  api.get('/country').then((response) => setPais(response.data))
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  function getCidade() {
+    api.get('/city').then((response) => setCidade(response.data))
+      .catch((err) => {
+        console.log(err);
+      })
+    }
   
 
   return (
     <div className="App">
-      <div Class="mt-3">
+      <div className="mt-3">
         <Container>
           <h6 className="font-weight-bold" class="text-primary">DESTINOS DE INTERESSE</h6>
           <Form >
 
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="disabledTextInput">Nome</Form.Label>
-                <Form.Control id="disabledTextInput"  />
-              </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="disabledTextInput">Nome</Form.Label>
+              <Form.Control id="disabledTextInput" />
+            </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="disabledTextInput">Email</Form.Label>
-                <Form.Control id="disabledTextInput" />
-              </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="disabledTextInput">Email</Form.Label>
+              <Form.Control id="disabledTextInput" />
+            </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="disabledTextInput">Telefone</Form.Label>
-                <Form.Control id="disabledTextInput"/>
-              </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="disabledTextInput">Telefone</Form.Label>
+              <Form.Control id="disabledTextInput" />
+            </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="disabledTextInput">CPF</Form.Label>
-                <Form.Control id="disabledTextInput"/>
-              </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="disabledTextInput">CPF</Form.Label>
+              <Form.Control id="disabledTextInput" />
+            </Form.Group>
 
-              <br />
-              <br />
+            <br />
+            <br />
 
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="disabledSelect">País</Form.Label>
-                <Form.Select id="disabledSelect">
-                  <option>1</option>
-                </Form.Select>
-              </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="disabledSelect">País</Form.Label>
+              <Form.Select id="disabledSelect">
+            {pais.map((pa)=> (
+                <option key={pa.code}>{pa.name}</option>
+             ))}
 
+              </Form.Select>
+            </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="disabledSelect">Cidade</Form.Label>
-                <Form.Select id="disabledSelect">
-                  <option>2</option>
-                </Form.Select>
-              </Form.Group>
+            
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="disabledSelect">Cidade</Form.Label>
+              <Form.Select id="disabledSelect">
+            {cidade.map((cida)=> (
+                <option key={cida.id}>{cida.name}</option>
+             ))}
+
+              </Form.Select>
+            </Form.Group>
+            
 
           </Form>
         </Container >
@@ -63,5 +91,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
